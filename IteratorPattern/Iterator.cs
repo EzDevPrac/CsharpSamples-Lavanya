e74e7935d12857a7c1179df8cc94c42045ae52fd
Lavanya B <lavanya@gmail.com>
2020-05-01 20:13:30 +0530

    files

---
+++ IteratorPattern/Iterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorPattern
+{
+      public class Iterator 
+        {
+            private ConcreteCollection collection;
+            private int current = 0;
+            private int step = 1;
+
+            // Constructor
+            public Iterator(ConcreteCollection collection)
+            {
+                this.collection = collection;
+            }
+
+            // Gets first item
+            public Elempoyee First()
+            {
+                current = 0;
+                return collection.GetEmployee(current);
+            }
+
+            // Gets next item
+            public Elempoyee Next()
+            {
+                current += step;
+                if (!IsCompleted)
+                {
+                    return collection.GetEmployee(current);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            // Check whether iteration is complete
+            public bool IsCompleted
+            {
+                get { return current >= collection.Count; }
+            }
+        }
+}
+
---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IteratorPattern
+{
+        public class Program
+        {
+            static void Main()
+            {
+                // Build a collection
+                ConcreteCollection collection = new ConcreteCollection();
+
+                collection.AddEmployee(new Elempoyee("Lavanya", 100));
+                collection.AddEmployee(new Elempoyee("Tereena", 101));
+                collection.AddEmployee(new Elempoyee("Raju", 102));
+                collection.AddEmployee(new Elempoyee("Karan", 103));
+                collection.AddEmployee(new Elempoyee("Shashank", 104));
+
+                // Create iterator
+                Iterator iterator = collection.CreateIterator();
+
+                //looping iterator      
+                Console.WriteLine("Iterating over collection:");
+
+                for (Elempoyee emp = iterator.First(); !iterator.IsCompleted; emp = iterator.Next())
+                {
+                    Console.WriteLine("ID:"+emp.ID + " Name:"+emp.Name);
+                }
+                Console.Read();
+            }
+        }
+}
