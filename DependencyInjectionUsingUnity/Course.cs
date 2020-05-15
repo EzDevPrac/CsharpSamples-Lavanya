0c6019b6b1eaabd7a1dd4d1e685ede1ad546b745
Lavanya B <lavanya@gmail.com>
2020-05-15 09:40:08 +0530

    files

---
+++ DependencyInjectionUsingUnity/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionUsingUnity
+{
+        public class Course
+        {
+            public long CourseID { get; set; }
+            public long InstitutionID { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return "Course id :" + CourseID  + "\n Institution Id :" + InstitutionID + "\n Title :" + Title + "\n Description :" + Description;
+        }
+
+    }
+}
---
+++ DependencyInjectionUsingUnity/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+
+namespace DependencyInjectionUsingUnity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var container = new UnityContainer();
+            container.RegisterType<IInstitutionService, InstitutionService>();
+            container.RegisterType<ICourseService, CourseService>();
+
+            InstitutionService institution = container.Resolve<InstitutionService>();
+          Console.WriteLine( institution.GetInstitutionByID(1).ToString());
+
+            CourseService course = container.Resolve<CourseService>();
+
+            Console.WriteLine(course.GetCourseByID(2));
+        
+
+        }
+    }
+}
