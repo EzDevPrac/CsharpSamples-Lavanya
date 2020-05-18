9f3fca727ef198483164a5b6c4f32318154eed0e
Lavanya B <lavanya@gmail.com>
2020-05-18 10:02:56 +0530

    update

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] employeesArray = new string[5, 4]
+             {
+                {"101","Lavanya","SE","10000"},
+                {"102","Tereena","SE","20000"},
+                {"103","Karan","SSE","30000"},
+                {"104","Raju","SE","40000"},
+                {"105","Shashank","SSE","50000"}
+             };
+
+            ITarget target = new EmployeeAdapter();
+            Console.WriteLine("HR system passes employee string array to Adapter\n");
+            target.ProcessCompanySalary(employeesArray);
+            Console.Read();
+        }
+    }
+}
---
+++ AdapterPattern/ThirdPartyBillingSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterPattern
+{
+        public class ThirdPartyBillingSystem
+        {
+           
+            public void ProcessSalary(List<Employee> listEmployee)
+            {
+                foreach (Employee employee in listEmployee)
+                {
+                    Console.WriteLine("Rs." + employee.Salary + " Salary Credited to " + employee.Name + " Account");
+                }
+            }
+        }
+}
