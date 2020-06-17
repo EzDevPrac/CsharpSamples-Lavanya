8af3c9d5603bead30333698aa32682a4b0802507
Lavanya B <lavanya@gmail.com>
2020-06-17 12:30:23 +0530

    ass

---
+++ Problem8/Point3D.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem8
+{
+     public class Point3D
+     {
+        private double x, y, z;
+        public Point3D(double x, double y,double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public void SetX(double n)
+        {
+            x = n;
+
+        }
+        public double GetX()
+        {
+            return x;
+        }
+        public void SetY(double n)
+        {
+            y = n;
+
+        }
+        public double GetY()
+        {
+            return y;
+        }
+        public void SetZ(double n)
+        {
+            z = n;
+
+        }
+        public double GetZ()
+        {
+            return z;
+        }
+        public void MoveTO(double num1, double num2,double num3 )
+        {
+            SetX(num1);
+            SetY(num2);
+            SetZ(num3);
+
+        }
+        public override string ToString()
+        {
+            return "(" + GetX() + "," + GetY() + "," + GetZ() + ")";
+        }
+        public double DistanceTo(Point3D p2)
+        {
+            return Math.Sqrt((x - p2.GetX()) * (x - p2.GetX()) +
+              (y - p2.GetY()) * (y - p2.GetY()) +
+              (z - p2.GetZ()) * (z - p2.GetZ()));
+        }
+    }
+}
---
+++ Problem8/TestPoint3D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem8
+{
+    class TestPoint3D
+    {
+        static void Main(string[] args)
+        {
+            
+            Point3D[] points = new Point3D[5];
+            points[0] = new Point3D(3, 4, 5);
+            points[1] = new Point3D(5, 6, 7);
+            points[2] = new Point3D(8, 3, 2);
+            points[3] = new Point3D(1, 4, 9);
+            points[4] = new Point3D(1, 4, -7);
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine(points[i].ToString());
+                Console.WriteLine(points[i].DistanceTo(points[0]));
+            }
+
+
+        }
+    }
+}
