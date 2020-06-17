3cde34d5eb217dea885dd6676af97bf2c14b929e
Lavanya B <lavanya@gmail.com>
2020-06-17 12:28:16 +0530

    ass

---
+++ Problem5/ComplexNumber.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem5
+{
+   public class ComplexNumber
+    {
+
+        private int Real { get; set; }
+        private int Imaginary { get; set; }
+
+        public ComplexNumber(int real, int imag)
+        {
+            Real = real;
+            Imaginary = imag;
+        }
+        public int GetReal()
+        {
+            return Real;
+        }
+        public void SetReal(int n)
+        {
+            Real = n;
+        }
+        public int GetImaginary()
+        {
+            return Imaginary;
+        }
+        public void SetImag(int n)
+        {
+            Imaginary = n;
+        }
+        public override string ToString()
+        {
+            return "(" + Real + "," + Imaginary + ")";
+        }
+        public double GetMagnitude()
+        {
+            double result = Math.Sqrt((Real * Real + Imaginary * Imaginary));
+            return result;
+        }
+        public void AddComplex(ComplexNumber number)
+        {
+
+            Real += number.GetReal();
+            Imaginary += number.GetImaginary();
+          
+        }
+        public static ComplexNumber operator +(ComplexNumber number1 , ComplexNumber number2)
+        {
+            int a = number1.GetReal() + number2.GetReal();
+            int b = number1.GetImaginary() + number2.GetImaginary();
+            return new ComplexNumber(a, b);
+        }
+        public static ComplexNumber operator -(ComplexNumber number1, ComplexNumber number2)
+        {
+            int a = number1.GetReal() - number2.GetReal();
+            int b = number1.GetImaginary() - number2.GetImaginary();
+            return new ComplexNumber(a, b);
+        }
+
+
+    }
+}
---
+++ Problem5/TestComplexNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem5
+{
+     class TestComplexNumber
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber number = new ComplexNumber(5, 2);
+            Console.WriteLine("Number is: " + number.ToString());
+
+            number.SetImag(-3);
+            Console.WriteLine("Number is: " + number.ToString());
+
+            Console.Write("Magnitude is: ");
+            Console.WriteLine(number.GetMagnitude());
+
+            ComplexNumber number2 = new ComplexNumber(-1, 1);
+            number.AddComplex(number2);
+            Console.Write("After adding: ");
+            Console.WriteLine(number.ToString());
+
+        }
+    }
+}
