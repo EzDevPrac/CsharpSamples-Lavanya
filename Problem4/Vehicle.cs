537a60c84727d84a896c5812117bf406820fdadd
Lavanya B <lavanya@gmail.com>
2020-06-17 12:27:55 +0530

    ass

---
+++ Problem4/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem4
+{
+     public class Vehicle
+     {
+        protected string Model { get; set; }
+        protected string Year { get; set; }
+        protected double Price { get; set; }
+
+        public void SetModel(string model)
+        {
+            Model = model;
+        }
+        public void SetYear(string year)
+        {
+            Year = year;
+        }
+        public void SetPrice(double price)
+        {
+            Price = price;
+        }
+
+        public void Strat()
+        {
+
+        }
+        public void Stop()
+        {
+
+        }
+
+
+    }
+}
